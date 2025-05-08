290ebbf01ca57bb42f6338a4860f96d4ee1cea33
Jinling Su <Jinling.Su@verisilicon.com>
2025-05-08 16:20:10 +0800

    Step9

--- Step9/test.sh
+++ Step9/test.sh
@@ -8,9 +8,12 @@ cmake --install . --prefix "./installdir"
 ctest -N
 ctest [-VV] -C Debug -D Experimental
 cpack
+cpack --config CPackSourceConfig.cmake
 # 2. test
-# cd installdir/bin
+cd installdir/bin
+./Tutorial 100
+./Tutorial || true
 tar -xzvf ./Tutorial-1.0-Linux.tar.gz
 ls -l
-# ./Tutorial 100
-# ./Tutorial || true
+./Tutorial-1.0-Linux 100
+./Tutorial-1.0-Linux || true
