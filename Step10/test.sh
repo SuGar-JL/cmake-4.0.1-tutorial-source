ade08e071be7001abeac9884230dc11647cf8037
Jinling Su <Jinling.Su@verisilicon.com>
2025-05-09 10:19:59 +0800

    Step10

--- Step10/test.sh
+++ Step10/test.sh
@@ -11,13 +11,13 @@ cpack
 cpack --config CPackSourceConfig.cmake
 # 2. test
 # Linux 动态链接器默认只在标准库路径（如 /usr/lib）和程序的当前目录下查找 .so 文件
-cp ./installdir/lib/libTutorial.so ./installdir/bin
+cp ./installdir/lib/libMathFunctions.so ./installdir/bin
 ls -l ./installdir
 ./installdir/bin/Tutorial 100
 ./installdir/bin/Tutorial || true
 
 tar -xzvf ./Tutorial-1.0-Linux.tar.gz
-cp ./Tutorial-1.0-Linux/lib/libTutorial.so ./Tutorial-1.0-Linux/bin
+cp ./Tutorial-1.0-Linux/lib/libMathFunctions.so ./Tutorial-1.0-Linux/bin
 ls -l ./Tutorial-1.0-Linux
 ./Tutorial-1.0-Linux/bin/Tutorial 100
 ./Tutorial-1.0-Linux/bin/Tutorial || true
