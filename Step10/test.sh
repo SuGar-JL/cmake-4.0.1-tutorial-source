67a8213331893bc8cba546f823607e1a12d39840
Jinling Su <Jinling.Su@verisilicon.com>
2025-05-09 10:19:59 +0800

    Step10

--- Step10/test.sh
+++ Step10/test.sh
@@ -15,6 +15,7 @@ ls -l .
 # 2. test with install dir
 # Linux 动态链接器默认只在标准库路径（如 /usr/lib）和程序的当前目录下查找 .so 文件
 cp ./installdir/lib/libMathFunctions.so ./installdir/bin
+cp ./installdir/lib/libSqrtLibrary.a ./installdir/bin
 tree ./installdir
 cd ./installdir/bin
 ./Tutorial 100
@@ -24,6 +25,7 @@ cd ../..
 # 3. test with cpack
 tar -xzvf ./Tutorial-1.0-Linux.tar.gz
 cp ./Tutorial-1.0-Linux/lib/libMathFunctions.so ./Tutorial-1.0-Linux/bin
+cp ./Tutorial-1.0-Linux/lib/libSqrtLibrary.a ./Tutorial-1.0-Linux/bin
 tree ./Tutorial-1.0-Linux
 cd ./Tutorial-1.0-Linux/bin
 ./Tutorial 100
