5e2f3c9b5c8cf0e75eaafa73f7b9db66d2f7af9b
Jinling Su <Jinling.Su@verisilicon.com>
2025-05-09 10:19:59 +0800

    Step10

--- Step10/test.sh
+++ Step10/test.sh
@@ -10,23 +10,14 @@ ctest [-VV] -C Debug -D Experimental
 cpack
 cpack --config CPackSourceConfig.cmake
 ls -l .
-./Tutorial 100
-./Tutorial || true
+whereis Tutorial
 # 2. test with install dir
-# Linux 动态链接器默认只在标准库路径（如 /usr/lib）和程序的当前目录下查找 .so 文件
-cp ./installdir/lib/libMathFunctions.so ./installdir/bin
-cp ./installdir/lib/libSqrtLibrary.a ./installdir/bin
-tree ./installdir
-cd ./installdir/bin
-./Tutorial 100
-./Tutorial || true
+Tutorial 100
+Tutorial || true
 
-cd ../..
+# Linux 动态链接器默认只在标准库路径（如 /usr/lib）和程序的当前目录下查找 .so 文件
 # 3. test with cpack
 tar -xzvf ./Tutorial-1.0-Linux.tar.gz
-cp ./Tutorial-1.0-Linux/lib/libMathFunctions.so ./Tutorial-1.0-Linux/bin
-cp ./Tutorial-1.0-Linux/lib/libSqrtLibrary.a ./Tutorial-1.0-Linux/bin
 tree ./Tutorial-1.0-Linux
-cd ./Tutorial-1.0-Linux/bin
-./Tutorial 100
-./Tutorial || true
+Tutorial-1.0-Linux/bin/Tutorial 100
+Tutorial-1.0-Linux/bin/Tutorial || true
